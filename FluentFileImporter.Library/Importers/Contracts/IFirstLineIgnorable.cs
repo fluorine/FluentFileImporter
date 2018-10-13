999a780953fae3f4e65e0052f5a0ef2f930b4459
Fluorine <joseph.anglada92@gmail.com>
2018-10-13 13:22:45 -0400

    Interface for importers able to ignore first line

---
+++ FluentFileImporter.Library/Importers/Contracts/IFirstLineIgnorable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentFileImporter.Library.Importers.Contracts
+{
+    public interface IFirstLineIgnorable<out T>
+    {
+        /// <summary>
+        /// The first row or line of file shall be ignored; e.g. a header.
+        /// False by default.
+        /// </summary>
+        bool FirstLineIgnored { get; }
+
+        /// <summary>
+        /// Configure importer to consider or ignore first line.
+        /// </summary>
+        /// <param name="ignore">
+        ///   True to ignore, default value if this function is invoked without arguments.
+        ///   False if explicitly stated in argument, but also false if this function is never invoked.</param>
+        /// <returns></returns>
+        T IgnoringFirstLine(bool ignore);
+    }
+}
